f690a97323e47a3dc825138a7cfbda938894ab88
omar-hossam <omarhossam50@proton.me>
2026-03-24 14:59:21 +0200

    feat(web): connect UnoCSS and clean src folder

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,5 @@
+export default function App() {
+  return (
+    <h1 bg="blue-600" text="white" rounded="lg" p="5">Hello from Noqta!</h1>
+  )
+}
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
+import UnoCSS from 'unocss/vite';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()],
+	plugins: [
+		UnoCSS({
+			configFile: './uno.config.ts',
+		}),
+		preact(),
+	],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@comps': path.resolve(__dirname, './src/components'),
+			'@types': path.resolve(__dirname, './src/types'),
+			'@hooks': path.resolve(__dirname, './src/hooks'),
+			'@assets': path.resolve(__dirname, './src/assets'),
+			'@images': path.resolve(__dirname, './src/assets/images'),
+		}
+	}
 });
