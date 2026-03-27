2cbfe64fd42da1b087c66dff86368e840f3dd7ca
omar-hossam <omarhossam50@proton.me>
2026-03-27 17:35:28 +0200

    feat: implement login page for user

--- routes/front.py
+++ routes/front.py
@@ -4,8 +4,12 @@ front_bp = Blueprint('front', __name__)
 
 @front_bp.route('/', methods=['GET'])
 def home():
-    return render_template('index.html', title='Home')
+    return render_template('index.html', is_home_page=True)
 
 @front_bp.route('/api/hello', methods=['GET'])
 def hello():
     return '<div>تم احضار البيانات بنجاح 🎉</div>'
+
+@front_bp.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html')
